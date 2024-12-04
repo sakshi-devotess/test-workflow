0a246f2ce3c52c9bb345a6f1b108706d322aa76f
Sakshi Antala <sakshi@saasinnova.com>
2024-12-04 14:45:05 +0530

    modify pr

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 1125
+          Learn React 1125656
         </a>
       </header>
     </div>
